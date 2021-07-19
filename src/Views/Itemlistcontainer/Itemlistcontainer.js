fb0984d8292a080fca199cc57dae55ce70fec668
AndresSubero <andressuberoh@gmail.com>
2021-07-18 21:27:35 -0300

    agrego filtro sin filtrar

--- src/Views/Itemlistcontainer/Itemlistcontainer.js
+++ src/Views/Itemlistcontainer/Itemlistcontainer.js
@@ -2,16 +2,38 @@ import React from "react"
 import ItemList from '../Itemlist/Itemlist';
 import { ItemsContext } from '../Context/Context';
 import  { useContext } from 'react';
-
+import  { useEffect } from 'react';
+import "./ItemListContainer.css"
 
 
 const Itemlistcontainer = () => {
 
-    const [items, setitems] = useContext(ItemsContext)
+    const [items, setItems] = useContext(ItemsContext)
 
 
     return (
-       <div><ItemList items={items}/>
+       <div>
+
+           <div className="menu-filter">
+               <h2>
+                   All
+               </h2>
+               <h2>
+                   CrossFit
+               </h2>
+               <h2>
+                   Barbells
+               </h2>
+               <h2>
+                   Racks
+               </h2>
+               <h2>
+                   Plates
+               </h2>
+
+           </div>
+           
+          <div><ItemList items={items}/></div> 
 
        
        
